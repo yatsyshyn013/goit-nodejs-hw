b6b57bb75788829ac26a2565a51d24de5fc77384
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-07-15 01:42:43 +0300

    updt

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = "mongodb+srv://Maksym:Ag5BU2QWGwqEIyjh@cluster0.ppwy3od.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
