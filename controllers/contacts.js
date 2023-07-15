c38073246a8eda6010dc243fefe05055562dda6a
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-07-15 16:25:10 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,11 +1,8 @@
 
-
-// const contacts = require('../models/contacts')
-
 const {Contact} = require("../models/contact");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
-// const { get } = require("../routes/api/contacts");
+
 
 
 
@@ -24,12 +21,6 @@ const getById = async (req, res, next) => {
     const result = await Contact.findById(id);
     if (!result) {
         throw HttpError(404, "Not found");
-        // const error = new Error("Not found");
-        // error.status = 404;
-        // throw error;
-        // return res.status(404).json({
-        //   message: "Not found"
-        // }) 
     };
 
     res.json(result)
@@ -38,7 +29,7 @@ const getById = async (req, res, next) => {
 
 const add = async (req, res, next) => { 
       
-        const result = await Book.create(req.body);
+        const result = await Contact.create(req.body);
         res.status(201).json(result)
 };
 
@@ -61,7 +52,6 @@ const updateFavorite = async (req, res, next) => {
 
     
     const { id } = req.params;
-    // const { favorite } = req.body;
     const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
    
     if (!result) {
